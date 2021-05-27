c36fa617938b8e18b7339655b679a28454470dba
Elie <goelie97@gmail.com>
2021-05-27 14:22:17 +0400

    initialized binding variables + added dependencies + added layout in activity_main.xml

--- app/src/main/java/com/example/raylierecipes/MainActivity.kt
+++ app/src/main/java/com/example/raylierecipes/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.raylierecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.raylierecipes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

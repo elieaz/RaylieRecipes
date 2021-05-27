c4981a7114e373dec19cae688b80c848976929b0
Elie <goelie97@gmail.com>
2021-05-27 17:07:26 +0400

    login page layout - made it landing page on AndroidManifest.xml

---
+++ app/src/main/java/com/example/raylierecipes/Login.kt
@@ -0,0 +1,15 @@
+package com.example.raylierecipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.raylierecipes.databinding.ActivityMainBinding
+
+class Login : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}

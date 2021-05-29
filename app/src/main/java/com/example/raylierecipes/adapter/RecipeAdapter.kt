f12df7d75ca1478dcaff1a801c700c3d6ba7405a
rayfreiha <rdfreiha@ndu.edu.lb>
2021-05-29 13:28:13 +0300

    Error with RecipeAdapter

---
+++ app/src/main/java/com/example/raylierecipes/adapter/RecipeAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.raylierecipes.adapter
+
+import com.example.raylierecipes.RecyclerView
+
+class RecipeAdapter {
+
+    inner class RecipeViewHolder(v:ItemListBinding): RecyclerView.ViewHolder(v.root){
+
+    }
+
+}

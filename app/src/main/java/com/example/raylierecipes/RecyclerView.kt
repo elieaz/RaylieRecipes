82d1df712ec885952bc2887118af8848a75113c6
rayfreiha <rdfreiha@ndu.edu.lb>
2021-05-29 11:26:17 +0300

    Recyclerview design done

--- app/src/main/java/com/example/raylierecipes/RecyclerView.kt
+++ app/src/main/java/com/example/raylierecipes/RecyclerView.kt
@@ -3,10 +3,12 @@ package com.example.raylierecipes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.raylierecipes.databinding.ActivityMainBinding
+import com.example.raylierecipes.databinding.ActivityRecyclerViewBinding
+import com.example.raylierecipes.model.RecipeData
 
 class RecyclerView : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityRecyclerViewBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
